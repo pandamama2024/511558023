b6cc565c89eb8b90003e4be6291e23e4c6db9818
pandamama2024 <jeanchien.cs11@nycu.edu.tw>
2024-03-11 23:03:07 +0800

    Modify main_test.js for LAB1

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -1,63 +1,41 @@
-const test = require("node:test");
-const assert = require("assert");
-const { MyClass, Student } = require("./main");
+const test = require('node:test');
+const assert = require('assert');
+const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
-    // TODO
     const myClass = new MyClass();
-    
-    // Test adding a non-Student object
-    assert.strictEqual(myClass.addStudent(1), -1);
-
-    // Test adding a valid Student object
     const student = new Student();
-    student.setName('Judy');
-    assert.strictEqual(myClass.addStudent(student), 0);
-    
-    // Test retrieving the added student by ID
+    student.setName('John');
+    const result = myClass.addStudent(student);
+
+    assert.strictEqual(result, 0);
+    assert.strictEqual(myClass.students.length, 1);
     assert.strictEqual(myClass.getStudentById(0), student);
 });
 
 test("Test MyClass's getStudentById", () => {
-    // TODO
     const myClass = new MyClass();
-    const student = new Student();
-    student.setName('Judy');
-    myClass.addStudent(student);
-    
-    // Test retrieving an existing student by valid ID
-    assert.strictEqual(myClass.getStudentById(0), student);
-    
-    // Test retrieving a student with an out-of-range ID
-    assert.strictEqual(myClass.getStudentById(myClass.students.length), null);
-    
-    // Test retrieving a student with a negative ID
+    const student1 = new Student();
+    student1.setName('John');
+    const student2 = new Student();
+    student2.setName('Jane');
+    myClass.addStudent(student1);
+    myClass.addStudent(student2);
+
+    assert.strictEqual(myClass.getStudentById(0), student1);
+    assert.strictEqual(myClass.getStudentById(1), student2);
     assert.strictEqual(myClass.getStudentById(-1), null);
+    assert.strictEqual(myClass.getStudentById(2), null);
 });
 
 test("Test Student's setName", () => {
-    // TODO
     const student = new Student();
-    
-    // Test setting a non-string name
-    assert.strictEqual(student.setName(123), undefined);
-    
-    // Test retrieving the name after setting it to a non-string value
-    assert.strictEqual(student.getName(), '');
-    
-    // Test setting and retrieving a valid name
-    student.setName('Judy');
-    assert.strictEqual(student.getName(), "Judy");
+    student.setName('John');
+    assert.strictEqual(student.name, 'John');
 });
 
 test("Test Student's getName", () => {
-    // TODO
     const student = new Student();
-    
-    // Test retrieving the name before setting it
-    assert.strictEqual(student.getName(), '');
-    
-    // Test setting and retrieving a valid name
-    student.setName('Judy');
-    assert.strictEqual(student.getName(), "Judy");
+    student.setName('John');
+    assert.strictEqual(student.getName(), 'John');
 });
